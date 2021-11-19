d4f0af773be67e629030eb82cf734922e5d65e23
leo <leopradanaadrian@ymail.com>
2021-11-19 22:09:42 +0700

    first

---
+++ admin/partials/footer.php
@@ -0,0 +1,10 @@
+<!-- Footer Section Starts -->
+<div class="footer">
+            <div class="wrapper">
+            <p class="text-center">Food Store Backend</a></p>
+            </div>
+        </div>
+        <!-- Footer Section Ends -->
+
+    </body>
+</html>
---
+++ partials-front/footer.php
@@ -0,0 +1,20 @@
+
+
+    <!-- footer Section Starts Here -->
+    <section class="footer">
+        <div class="container text-center">
+            <h4>About Us</h4>
+            <p>Food Store is a web that aims to make it easier for people to buy food without having to leave the house, and only use <br> their fingers to meet your daily energy needs. </p>
+            <div class="icons">
+                <i class= "fab fa-facebook"></i>
+                <i class= "fab fa-twitter"></i>
+                <i class= "fab fa-instagram"></i>
+                <i class= "fab fa-linkedin"></i>
+            </div>
+            <p>Made with <i class= "fa fa-heart-o"></i> by Food-store</p>
+        </div>
+    </section>
+    <!-- footer Section Ends Here -->
+
+</body>
+</html>
